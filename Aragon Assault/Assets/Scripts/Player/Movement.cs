7ba3b33298b20332027ea89d6038d897fccf8f45
Arnav3067 <22akothari.acc@lwetb.ie>
2024-02-28 22:55:46 +0000

    Completed the movement script for the player controller

--- Aragon Assault/Assets/Scripts/Player/Movement.cs
+++ Aragon Assault/Assets/Scripts/Player/Movement.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     [SerializeField] private float linearVelocity = 20f;
+    [SerializeField] private float angularVelocityVertical = -15f;
+    [SerializeField] private float angularVelocityHorizontal = -3f;
+    [SerializeField] private float axialAngularVelocity = 2f;
     [SerializeField] private float clampRangeX = 10f;
-    [SerializeField] private float clampRangeY = 8f;
+    [SerializeField] private float clampRangeY = 7f;
 
     private PlayerInputReceiver inputs;
 
@@ -26,7 +29,13 @@ public class Movement : MonoBehaviour
         _handleRotations(xInputs, yInputs);
 
         void _handleRotations(float xInputs, float yInputs) {
-            
+
+            float pitch = transform.localPosition.y * angularVelocityVertical + yInputs;
+            float yaw = transform.localPosition.x * axialAngularVelocity;
+            float roll = transform.localPosition.x * angularVelocityHorizontal + xInputs;
+
+            transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+
         }
 
         void _handleLinearMotion(float xInputs, float yInputs)
@@ -37,8 +46,7 @@ public class Movement : MonoBehaviour
 
             // gets clamped direction of y axis
             float yDir = yInputs * linearVelocity * Time.deltaTime;
-            float yMin = -1f; float yMax = 9f;
-            float clampedYDir = Mathf.Clamp(transform.localPosition.y + yDir, yMin, yMax);
+            float clampedYDir = Mathf.Clamp(transform.localPosition.y + yDir, -clampRangeY, clampRangeY);
 
             // applies it to the local position
             transform.localPosition = new Vector3(clampedXDir, clampedYDir, 0);
